e5ddd1b5dd9b59273a263db41a01edfedbda3aa0
Jaazbe <jaazbeh@gmail.com>
2020-11-09 22:45:00 -0800

    Creating basic pages

---
+++ gatsby/src/pages/404.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function fourOhFourPage(){
+    return (
+        <div>
+            <p>This page cannot be found.</p>
+        </div>
+    );
+}
---
+++ gatsby/src/pages/slicemasters.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function SlicemastersPage(){
+    return (
+        <div>
+            <p>Hello from the Slice masters page.</p>
+        </div>
+    );
+}
