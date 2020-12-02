9a39ca94a04a0610ec9becfd31b411b50c605294
Zahra Behfarshad <zbehfarshad@gmail.com>
2020-12-01 22:48:54 -0800

    Removing div and placing react fragment

--- gatsby/src/pages/slicemasters.js
+++ gatsby/src/pages/slicemasters.js
@@ -2,8 +2,8 @@ import React from 'react';
 
 export default function SlicemastersPage() {
   return (
-    <div>
+    <>
       <p>Hello from the Slice masters page.</p>
-    </div>
+    </>
   );
 }
