418c1f0669694829d77383779ec4770fda348f08
Zahra Behfarshad <zbehfarshad@gmail.com>
2020-12-02 21:45:05 -0800

    Using styled-components for styling + defining the global styles and importing them in layout along with normalize.css

--- gatsby/src/components/Layout.js
+++ gatsby/src/components/Layout.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import Nav from './Nav';
 import Footer from './Footer';
+import 'normalize.css';
+import GlobalStyles from '../styles/GlobalStyles';
 
 export default function Layout(props) {
   const { children } = props;
   return (
     <div>
+      <GlobalStyles />
       <Nav />
       {children}
       <Footer />
