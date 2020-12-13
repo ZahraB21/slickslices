1738d58560b31fe1db45b366d7230b9700b3d426
Zahra Behfarshad <zbehfarshad@gmail.com>
2020-12-13 12:33:57 -0800

    Adding styles to the layout component

--- gatsby/src/components/Layout.js
+++ gatsby/src/components/Layout.js
@@ -1,19 +1,27 @@
 import React from 'react';
+import styles from 'styled-components';
 import Nav from './Nav';
 import Footer from './Footer';
 import 'normalize.css';
 import GlobalStyles from '../styles/GlobalStyles';
 import Typography from '../styles/Typography';
 
+const ContentStyles = styles.div`
+  padding: 2rem;
+  background: white;
+`;
+
 export default function Layout(props) {
   const { children } = props;
   return (
-    <div>
+    <>
       <GlobalStyles />
       <Typography />
-      <Nav />
-      {children}
+      <ContentStyles>
+        <Nav />
+        {children}
+      </ContentStyles>
       <Footer />
-    </div>
+    </>
   );
 }
