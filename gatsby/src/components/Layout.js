d6f61c3e9bdd23194ab79a3dcd1ae6162fe548a7
Zahra Behfarshad <zbehfarshad@gmail.com>
2020-12-13 13:18:09 -0800

    Styling the border via styled components and integrating it in the Layout

--- gatsby/src/components/Layout.js
+++ gatsby/src/components/Layout.js
@@ -1,12 +1,28 @@
 import React from 'react';
-import styles from 'styled-components';
+import Styles from 'styled-components';
 import Nav from './Nav';
 import Footer from './Footer';
 import 'normalize.css';
 import GlobalStyles from '../styles/GlobalStyles';
 import Typography from '../styles/Typography';
+import stripes from '../assets/images/stripes.svg';
 
-const ContentStyles = styles.div`
+const SiteBorderStyles = Styles.div`
+  max-width: 1000px;
+  margin: 12rem auto 4rem auto;
+  margin-top: clamp(2rem, 10vw, 12rem );
+  border: 5px solid white;
+  background: white url(${stripes});
+  padding: 5px;
+  padding: clamp(5px, 1vw, 25px);
+  box-shadow: 0 0 5px 3px rgba(0, 0, 0, 0.44);
+  @media (max-width: 1100px) {
+    margin-left: 1.5rem;
+    margin-right: 1.5rem;
+  }
+`;
+
+const ContentStyles = Styles.div`
   padding: 2rem;
   background: white;
 `;
@@ -17,10 +33,12 @@ export default function Layout(props) {
     <>
       <GlobalStyles />
       <Typography />
-      <ContentStyles>
-        <Nav />
-        {children}
-      </ContentStyles>
+      <SiteBorderStyles>
+        <ContentStyles>
+          <Nav />
+          {children}
+        </ContentStyles>
+      </SiteBorderStyles>
       <Footer />
     </>
   );
